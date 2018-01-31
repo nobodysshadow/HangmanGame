99985f58001b1355056278a20b0ae48e80101306
Nobodysshadow <guenther.wasser@gmail.com>
2018-01-31 16:32:32 +0100

    Inital Setup

--- Python/hangmanGame.py
+++ Python/hangmanGame.py
@@ -2,9 +2,3 @@
 [hangmanGame.py] from the HangmanGame
 Main application for the game
 """
-# Import necessary modules and the wordlist
-import modulGW.py
-import modulIW.py
-import wordList.py
-
-# Start of the game

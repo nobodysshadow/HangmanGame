f7c820196c5feeb7e42c3667a2523fa6d68b1afe
LarsZauberer <wasser.ian@gmail.com>
2018-02-03 10:44:50 +0100

    Game_library V1
    I have added many functions to the game library. It has no Hangman sign. This
    will come soon.

--- Python/Lib/game_library.py
+++ Python/Lib/game_library.py
@@ -2,6 +2,11 @@
 The main game library for the game "HangmanGame"
 """
 def guess_a_letter(guessed_letters, tries):
+    """
+    Function to guess a letter, remove a try and add the letter to the letter list.
+    guessed_letter: The guessed letters.
+    tries: The Number of tries.
+    """
     guess_possible = 0
     while guess_possible == 0:
         guess = input()
@@ -17,10 +22,70 @@ def guess_a_letter(guessed_letters, tries):
     output = [tries, guessed_letters.append(guess)]
     return output
 
-def show(guessed_letters, number_of_letters, list_of_letters_in_the_word):
+def show(guessed_letters, word):
+    """
+    Shows the letters.
+    guessed_letters: The guessed letters.
+    word: The secret word.
+    """
     letter_list = []
-    while number_of_letters == 0:
-        for i in guessed_letters:
-            for l in list_of_letters_in_the_word:
-                letter_list.append
+    for i in guessed_letters:
+        for letters_in_word in list(word):
+            if i == letters_in_word:
+                letter_list.append(i + " ")
+            else:
+                letter_list.append("_ ")
+    print(letter_list)            
+    return letter_list
+
+def check_guess(guess, word, tries, guessed_letters, letter_list):
+    """
+    guess: The player guess.
+    word: The secret word.
+    tries: Number of tries.
+    guessed_letters: The guessed letters.
+    letter_list: The word with the gaps.
+    """
+    win = 0
+    if len(list(guess)) > 0:
+        if guess == word:
+            win = 1
+        else:
+            lose(tries)
+    else:
+        for i in list(word):
+            if i == guess:
+                right_guess(word, guessed_letters, letter_list)
+    return win
+
+def lose(tries):
+    """
+    tries: The number of tries
+    It show a little lose message and check the tries.
+    """
+    if tries == 0:
+        print("Du hast verloren")
+    else:
+        print("Du hast falsch geraten")
     return None
+
+def right_guess(word, guessed_letters, letter_list):
+    """
+    word: The secret word.
+    guessed_letters: The guessed letters.
+    letter_list: The word with the gaps.
+    This function let you win.
+    """
+    win = 1
+    win_list = []
+    print("Du hast richtig geraten")
+    for i in list(word):
+        for letters in guessed_letters:
+            if i == letters:
+                win_list.append(1)
+            else:
+                win_list.append(0)
+    for i in win_list:
+        if i != 1:
+            win = 0
+    return win
